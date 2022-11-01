a5b11269cf91b1878af6f384e6ac4d616c6bea90
Christopher Alphonse <christopheralphonse96@gmail.com>
2022-10-31 23:34:03 -0700

    updating  cli cargo yarn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  clearScreen: false,
+
+  server: {
+    strictPort: true,
+  },
+
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    // Tauri supports es2021
+    target: ["es2021", "chrome100", "safari13"],
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+});
