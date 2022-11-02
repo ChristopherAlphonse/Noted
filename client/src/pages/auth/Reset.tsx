99291891ce62a506cea9a36976b465355ed67d3a
Christopher Alphonse <christopheralphonse96@gmail.com>
2022-11-01 22:35:31 -0700

    changed reset page

--- client/src/pages/auth/Reset.tsx
+++ client/src/pages/auth/Reset.tsx
@@ -51,8 +51,7 @@ const Reset = () => {
           href="#"
           className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-          <img className="w-10 h-10 mr-2" src={Logo} alt="logo" />
-          Noted
+          <img className="w-20 h-20 mr-2" src={Logo} alt="logo" />
         </a>
         <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
           <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
